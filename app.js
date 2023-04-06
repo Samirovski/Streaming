d2c631cb555e9dd7f351d58d4027df19c2290cdb
Samirovski <130086779+Samirovski@users.noreply.github.com>
2023-04-06 21:35:44 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,28 @@
 const express = require('express');
 const app = express(); 
 app.get('/SCRAPER0', (req0, res0) => {
+const rq = require('request');
+const rp = require('request-promise');
+const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
-})
+rp(url)
+  .then(function(html){
+    //success!
+    console.log(html);
+  })
+  .catch(function(err){
+    //handle error
+  });
+
+});
+
+//💔💙💚 Start the server
+         
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
+	
+	module.exports = app;
