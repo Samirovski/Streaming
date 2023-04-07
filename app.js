47c1552e2b89c440968feee337b825d21e9174c5
Samirovskit <130086779+Samirovskit@users.noreply.github.com>
2023-04-07 00:04:25 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,24 +5,25 @@ const app = express();
 app.get('/SCRAPER0', (req0, res0) => {
 const rq = require('request');
 const rp = require('request-promise');
+var HTMLParser = require('node-html-parser');
+	
 //const url = require('url');
 //var type = decodeURIComponent(url.format({ pathname: req.originalUrl })).split("☆")[1];
 		
 
-	//TO SCRAPE GTRENDS AUTOCOMPLETE & DAILY TRENDS & REAL TIME TRENDS
+	
 
 
 const url = 'https://cima4u2.shop/category/%d8%a7%d9%81%d9%84%d8%a7%d9%85-%d8%a7%d8%ac%d9%86%d8%a8%d9%8a-movies7-english/page/2/';
 
-rp(url)
-  .then(function(html){
-    //success!
-    res0.header('Content-Type', 'application/json').send(html);
-    //console.log(html);
+rp(url).then(function(html){
+	const doc = HTMLParser.parse(html);
+const nodes = doc.querySelectorAll('.MovieBlock a')
+   
+    res0.header('Content-Type', 'application/json').send(nodes);
+    
   })
-  .catch(function(err){
-    //handle error
-  });
+  
 
 });
 
